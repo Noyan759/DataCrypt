d40abcce55464762fa2013eabb754ef2e5965e4b
Noyan759 <noyanahmed96@gmail.com>
2018-02-17 01:17:48 +0500

    Working on hash/verify api

--- controller/routes.js
+++ controller/routes.js
@@ -7,16 +7,32 @@ var router = app.Router()
         res.render('home', {});
         
     });
-    router.post("/submit", function(req, res){        
-        Web3.storeFile(req.body.owner, req.body.hash, function (message) {
-            res.render('home', { message: message });
-        })
+    router.get('/addFile', function (req, res) {
+        res.render('AddFile', {});
+        
     });
-    router.get("/getInfo", function(req, res){
+    router.get("/verifyFile", function(req, res){
+        res.render('VerifyFile', { message: "FileHash is undefined" });
+    });
+    router.post("/verifyFile", function(req, res){ 
+        console.log("\nhash: "+req.body.hash);
+
+        if(req.body.hash===undefined)
+            res.render('VerifyFile', { message: "FileHash is undefined" });
         var fileHash = req.body.hash;
         Web3.getInfo(req.body.hash, function (info) {
-            res.render('fileinfo', { message: info });
+            res.render('VerifyFile', { message: info });
         })
+    });
+    router.post("/addFile", function(req, res){  
+        console.log("\nowner: " + req.body.owner + "\nhash: " + req.body.hash);
+        
+        if(req.body.owner===undefined || req.body.hash===undefined)
+            res.render('AddFile', { message: "Owner/FileHash is undefined" });
+        
+        // Web3.storeFile(req.body.owner, req.body.hash, function (message) {
+        //     res.render('home', { message: message });
+        // })
     });
 
 module.exports = router;
