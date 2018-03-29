81ae3f137aabaa08576c4ad02c327a43c4d74725
urwah1996 <mohammadurwah@gmail.com>
2018-03-29 15:53:31 +0500

    bcrypt added

--- controller/authenticateController.js
+++ controller/authenticateController.js
@@ -4,7 +4,8 @@ const express = require('express');
 const router = express.Router();
 
 const account = require('../models/DCAccountManagement');
-
+var bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 let validator;
 let jwt;
@@ -24,37 +25,40 @@ module.exports.construct = function (body_parser,app) {
             } else if (user) {
 
                 // check if password matches
-                if (user.password !== req.body.password) {
-                    res.json({success: false, message: 'Authentication failed. Wrong password.'});
-                } else {
+                bcrypt.compare(req.body.password, user.password).then(function(res) {
+                    // res == true
+                    if(!res)
+                    {
+                        res.json({success: false, message: 'Authentication failed. Wrong password.'});
+                    }
+                    else{
+                        // if user is found and password is right
+                        // create a token
+                        console.log("REACHED");
+                        let dateToday = new Date();
 
-                    // if user is found and password is right
-                    // create a token
-                    console.log("REACHED");
-                    let dateToday = new Date();
+                        let userDetails ={
+                            email: user.email,
+                            password: user.password,
+                            dateToday: dateToday
+                        };
+                        console.log(userDetails);
 
-                    let userDetails ={
-                        email: user.email,
-                        password: user.password,
-                        dateToday: dateToday
-                    };
-                    console.log(userDetails);
+                        let token = jwt.sign(userDetails, app.get('superSecret'), {
+                            // expiresIn: 60*60*24 // expires in 12 hours
+                        });
 
-                    let token = jwt.sign(userDetails, app.get('superSecret'), {
-                        // expiresIn: 60*60*24 // expires in 12 hours
-                    });
+                        // return the information including token as JSON
+                        res.json({
+                            success: true,
+                            message: 'Enjoy your token!',
+                            token: token,
+                            id: user.id
 
-                    // return the information including token as JSON
-                    res.json({
-                        success: true,
-                        message: 'Enjoy your token!',
-                        token: token,
-                        id: user.id
+                        });
 
-                    });
-
-                }
-
+                    }
+                });
             }
 
         });
